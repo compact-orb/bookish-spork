df50c7edf39b7e26c7b49e2b8d252d5a63bcc626
compact-orb <compact-orb@outlook.com>
2025-06-06 14:55:57 +0000

    Update

--- Invoke-Gentoo.ps1
+++ Invoke-Gentoo.ps1
@@ -4,14 +4,19 @@ param(
 
 switch ($Clean) {
     $true {
+        Remove-Item -Path /mnt/gentoo/usr/local/bin/pwsh
+
         umount /mnt/gentoo/mnt /mnt/gentoo/opt/microsoft
 
         Remove-Item -Path /mnt/gentoo/opt/microsoft
+        
     }
     $false {
         New-Item -Path /mnt/gentoo/opt/microsoft -ItemType Directory
 
         mount --bind /opt/microsoft /mnt/gentoo/opt/microsoft
+
+        New-Item -Path /mnt/gentoo/usr/local/bin/pwsh -ItemType SymbolicLink -Target /opt/microsoft/powershell/7/pwsh
 
         mount --bind $(Get-Location).Path /mnt/gentoo/mnt
     }
