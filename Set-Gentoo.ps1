06e80f33194f04dbd23414dbe0e3116358493df6
compact-orb <compact-orb@outlook.com>
2025-06-06 13:08:10 +0000

    Update

--- Set-Gentoo.ps1
+++ Set-Gentoo.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 Remove-Item -Path /mnt/gentoo/etc/kernel/config.d, /mnt/gentoo/etc/portage/env, /mnt/gentoo/etc/portage/package.env, /mnt/gentoo/etc/portage/package.use -Recurse -Force -ErrorAction SilentlyContinue
 
-Copy-Item -Path $env:CONFIG_PREFIX/* -Destination /mnt/gentoo -Recurse
+Copy-Item -Path $env:CONFIG_PREFIX/* -Destination /mnt/gentoo -Recurse -Force
 
 Set-Content -Path /mnt/gentoo/etc/portage/make.conf -Value (Get-Content -Path /mnt/gentoo/etc/portage/make.conf | ForEach-Object {
     if ($_ -match "^EMERGE_DEFAULT_OPTS=") {
