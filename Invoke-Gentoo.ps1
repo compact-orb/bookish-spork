2773ecf63dd9e50c1384e108cc677e14cd582c77
compact-orb <compact-orb@outlook.com>
2025-06-06 13:51:16 +0000

    Update

--- Invoke-Gentoo.ps1
+++ Invoke-Gentoo.ps1
@@ -18,7 +18,7 @@ mount --bind $(Get-Location).Path /mnt/gentoo/mnt
 
 $scriptPath = "/mnt/Invoke-GentooEmerge.ps1" + $(if ($Packages) { " -Packages $Packages" } else { "" }) + $(if ($Resume) { " -Resume" } else { "" }) + $(if ($Bootstrap) { " -Bootstrap" } else { "" }) + $(if ($PortageProfile) { " -PortageProfile $PortageProfile" } else { "" }) + $(if ($EmergePerl) { " -EmergePerl" } else { "" })
 
-chroot /mnt/gentoo /opt/microsoft/powershell/7/pwsh $scriptPath
+chroot /mnt/gentoo /opt/microsoft/powershell/7/pwsh -Command "$scriptPath"
 
 umount /mnt/gentoo/mnt /mnt/gentoo/opt/microsoft
 
