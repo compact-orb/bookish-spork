565aeec99e950af596f35aa4a1ff9a2d97636506
compact-orb <compact-orb@outlook.com>
2025-06-08 05:07:58 +0000

    Update

--- install-powershell.sh
+++ install-powershell.sh
@@ -2,7 +2,7 @@ set -e
 
 source ./variables.sh
 
-if [ $1 == "official" ]; then
+if [ "$1" = official ]; then
     aria2c --dir=/tmp https://github.com/PowerShell/PowerShell/releases/download/v7.$POWERSHELL_7_VERSION/powershell-7.$POWERSHELL_7_VERSION-linux-x64.tar.gz
 else
     aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $BUNNY_STORAGE_ACCESS_KEY" https://$BUNNY_STORAGE_ENDPOINT/$BUNNY_STORAGE_ZONE_NAME/powershell-7.$POWERSHELL_7_VERSION-linux-x64.tar.zst
@@ -10,7 +10,7 @@ fi
 
 mkdir --parents /opt/microsoft/powershell/7
 
-if [ $1 == "official" ]; then
+if [ "$1" = official ]; then
     tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.$POWERSHELL_7_VERSION-linux-x64.tar.gz
 
     rm /tmp/powershell-7.$POWERSHELL_7_VERSION-linux-x64.tar.gz
