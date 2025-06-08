0c9417bf62197cca893972d1b9ace9e3dbddfc28
compact-orb <compact-orb@outlook.com>
2025-06-08 03:03:51 +0000

    Update

--- install-powershell.sh
+++ install-powershell.sh
@@ -8,6 +8,8 @@ mkdir --parents /opt/microsoft/powershell/7
 
 tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.${POWERSHELL_7_VERSION}-linux-x64.tar.gz
 
+rm /tmp/powershell-7.${POWERSHELL_7_VERSION}-linux-x64.tar.gz
+
 chmod +x /opt/microsoft/powershell/7/pwsh
 
 ln --symbolic /opt/microsoft/powershell/7/pwsh /usr/local/bin/pwsh
