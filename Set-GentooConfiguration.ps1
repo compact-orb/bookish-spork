cd57eb86df93461daa1a89f5af9bb0f1ec35b8d2
compact-orb <compact-orb@outlook.com>
2025-06-12 23:53:14 +0800

    Update

--- Set-GentooConfiguration.ps1
+++ Set-GentooConfiguration.ps1
@@ -1,3 +1,7 @@
+param (
+    [switch]$Client
+)
+
 $ErrorActionPreference = "Stop"
 
 $PSNativeCommandUseErrorActionPreference = $true
@@ -6,15 +10,16 @@ Remove-Item -Path /mnt/gentoo/etc/kernel/config.d, /mnt/gentoo/etc/portage/env, 
 
 Copy-Item -Path $env:CONFIG_PREFIX/* -Destination /mnt/gentoo -Recurse -Force
 
-Set-Content -Path /mnt/gentoo/etc/portage/make.conf -Value (Get-Content -Path /mnt/gentoo/etc/portage/make.conf | ForEach-Object {
-    if ($_ -match "^EMERGE_DEFAULT_OPTS=") {
-        "# $_"
-    } else {
-        $_
-    }
-})
+if (-not $Client) {
+    Set-Content -Path /mnt/gentoo/etc/portage/make.conf -Value (Get-Content -Path /mnt/gentoo/etc/portage/make.conf | ForEach-Object {
+        if ($_ -match "^EMERGE_DEFAULT_OPTS=") {
+            "# $_"
+        } else {
+            $_
+        }
+    })
 
-Add-Content -Path /mnt/gentoo/etc/portage/make.conf -Value @'
+    Add-Content -Path /mnt/gentoo/etc/portage/make.conf -Value @'
 
 MAKEOPTS="--jobs=8 --load-average=9"
 
@@ -22,3 +27,4 @@ EMERGE_DEFAULT_OPTS="--buildpkg --quiet-build --usepkg"
 BINPKG_COMPRESS="zstd"
 BINPKG_COMPRESS_FLAGS="-22 -T8 --long --ultra"
 '@
+}
