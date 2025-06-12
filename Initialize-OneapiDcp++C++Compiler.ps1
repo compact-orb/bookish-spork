b714a730549b74a5d2409f9bac10e74fb3076c6c
compact-orb <compact-orb@outlook.com>
2025-06-12 20:13:22 +0800

    Update

--- Initialize-OneapiDcp++C++Compiler.ps1
+++ Initialize-OneapiDcp++C++Compiler.ps1
@@ -2,8 +2,10 @@ $ErrorActionPreference = "Stop"
 
 $PSNativeCommandUseErrorActionPreference = $true
 
-aria2c --dir=/tmp https://registrationcenter-download.intel.com/akdlm/IRC_NAS/c4d2aef3-3123-475e-800c-7d66fd8da2a5/intel-dpcpp-cpp-compiler-2025.1.1.9_offline.sh
+. ./Variables.ps1
 
-bash /tmp/intel-dpcpp-cpp-compiler-2025.1.1.9_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
+aria2c --dir=/tmp https://registrationcenter-download.intel.com/akdlm/IRC_NAS/c4d2aef3-3123-475e-800c-7d66fd8da2a5/intel-dpcpp-cpp-compiler-$oneapiDcpppCppCompiler_offline.sh
 
-Remove-Item -Path /tmp/intel-dpcpp-cpp-compiler-2025.1.1.9_offline.sh
+bash /tmp/intel-dpcpp-cpp-compiler-$oneapiDcpppCppCompiler_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
+
+Remove-Item -Path /tmp/intel-dpcpp-cpp-compiler-$oneapiDcpppCppCompiler_offline.sh
