310d225723392f2ca68ccd12ad6dff70a52bc205
compact-orb <compact-orb@outlook.com>
2025-06-12 20:30:50 +0800

    Update

--- Initialize-OneapiDcp++C++Compiler.ps1
+++ Initialize-OneapiDcp++C++Compiler.ps1
@@ -4,8 +4,8 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 . ./Variables.ps1
 
-aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $env:BUNNY_STORAGE_ACCESS_KEY" https://$env:BUNNY_STORAGE_ENDPOINT/$env:BUNNY_STORAGE_ZONE_NAME/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh
+aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $env:BUNNY_STORAGE_ACCESS_KEY" https://$env:BUNNY_STORAGE_ENDPOINT/$env:BUNNY_STORAGE_ZONE_NAME/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh
 
-bash /tmp/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
+bash /tmp/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
 
-Remove-Item -Path /tmp/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh
+Remove-Item -Path /tmp/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh
