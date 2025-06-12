838af2bd11a1544a80ba4226f3b20028eb809220
compact-orb <compact-orb@outlook.com>
2025-06-12 20:26:40 +0800

    Update

--- Initialize-OneapiDcp++C++Compiler.ps1
+++ Initialize-OneapiDcp++C++Compiler.ps1
@@ -4,7 +4,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 . ./Variables.ps1
 
-aria2c --dir=/tmp https://registrationcenter-download.intel.com/akdlm/IRC_NAS/c4d2aef3-3123-475e-800c-7d66fd8da2a5/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh
+aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $env:BUNNY_STORAGE_ACCESS_KEY" https://registrationcenter-download.intel.com/akdlm/IRC_NAS/c4d2aef3-3123-475e-800c-7d66fd8da2a5/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh
 
 bash /tmp/intel-dpcpp-cpp-compiler-($oneapiDcpppCppCompiler)_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
 
