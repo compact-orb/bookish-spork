f401999ea6c5e08fb41a41a513dedb5146bae7c0
compact-orb <compact-orb@outlook.com>
2025-06-15 17:09:45 +0800

    Update

--- Start-InstallOneapiDcp++C++Compiler.ps1
+++ Start-InstallOneapiDcp++C++Compiler.ps1
@@ -4,12 +4,10 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 . ./Variables.ps1
 
+Remove-Item -Path /mnt/gentoo/opt/intel/oneapi -Recurse -Force -ErrorAction SilentlyContinue
+
 aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $env:BUNNY_STORAGE_ACCESS_KEY" https://$env:BUNNY_STORAGE_ENDPOINT/$env:BUNNY_STORAGE_ZONE_NAME/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh
 
 bash /tmp/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh --silent -a --eula accept --ignore-errors --silent --install-dir /mnt/gentoo/opt/intel/oneapi
 
 Remove-Item -Path /tmp/intel-dpcpp-cpp-compiler-$($oneapiDcpppCppCompiler)_offline.sh
-
-Set-Content -Path /mnt/gentoo/etc/ld.so.conf.d/oneapi.conf -Value "/opt/intel/oneapi/compiler/latest/lib"
-
-chroot /mnt/gentoo env-update
