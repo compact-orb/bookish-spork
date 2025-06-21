adce2cd1f579a500ca7637f682fa93059d7f4da0
compact-orb <compact-orb@outlook.com>
2025-06-21 23:01:30 +0800

    Update

---
+++ Start-MountFilesystems.ps1
@@ -0,0 +1,31 @@
+param(
+    [switch]$Unmount
+)
+
+$ErrorActionPreference = "Stop"
+
+$PSNativeCommandUseErrorActionPreference = $true
+
+if ($Unmount) {
+    mount --types proc /proc /mnt/gentoo/proc
+
+    mount --rbind /sys /mnt/gentoo/sys
+
+    mount --make-rslave /mnt/gentoo/sys
+
+    mount --rbind /dev /mnt/gentoo/dev
+
+    mount --make-rslave /mnt/gentoo/dev
+
+    mount --bind /run /mnt/gentoo/run
+
+    mount --make-slave /mnt/gentoo/run
+
+    mount --bind /tmp /mnt/gentoo/tmp
+
+    mount --make-slave /mnt/gentoo/tmp
+
+    mount --bind ./ /mnt/gentoo/mnt
+} else {
+    umount --lazy /mnt/gentoo/dev /mnt/gentoo/proc /mnt/gentoo/run /mnt/gentoo/sys /mnt/gentoo/tmp /mnt/gentoo/mnt
+}
