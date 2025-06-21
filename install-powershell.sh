7bd1a12fafcd15b8a983a4360a1a040a157cb04b
compact-orb <compact-orb@outlook.com>
2025-06-21 09:13:42 +0000

    Update

--- install-powershell.sh
+++ install-powershell.sh
@@ -4,23 +4,13 @@ set -e
 
 source ./variables.sh
 
-if [ "$1" = official ]; then
-    aria2c --dir=/tmp https://github.com/PowerShell/PowerShell/releases/download/v7.$POWERSHELL_7_VERSION/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
-else
-    aria2c --dir=/tmp --header="accept: */*" --header="accesskey: $BUNNY_STORAGE_ACCESS_KEY" https://$BUNNY_STORAGE_ENDPOINT/$BUNNY_STORAGE_ZONE_NAME/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.zst
-fi
+aria2c --dir=/tmp https://github.com/PowerShell/PowerShell/releases/download/v7.$POWERSHELL_7_VERSION/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
 
 mkdir --parents /opt/microsoft/powershell/7
 
-if [ "$1" = official ]; then
-    tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
+tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
 
-    rm /tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
-else
-    tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.zst --use-compress-program="zstd --long=31"
-
-    rm /tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.zst
-fi
+rm /tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
 
 chmod +x /opt/microsoft/powershell/7/pwsh
 
