2617100b5c863642743710355cb99b5c10c4ff20
compact-orb <compact-orb@outlook.com>
2025-07-04 22:26:24 +0800

    Update

--- criu-restore.sh
+++ criu-restore.sh
@@ -1,6 +1,6 @@
 export FEATURES="-ipc-sandbox -network-sandbox -pid-sandbox"
 
-sudo criu restore --file-locks --images-dir /mnt/gentoo/var/lib/criu --shell-job &
+sudo criu restore --ext-unix-sk --file-locks --images-dir /mnt/gentoo/var/lib/criu --shell-job &
 
 pid=$!
 
@@ -11,7 +11,7 @@ sleep_pid=$!
 wait -n "$pid" "$sleep_pid"
 
 if sudo kill -0 $pid; then
-    criu dump --file-locks --images-dir /mnt/gentoo/var/lib/criu --shell-job --tree "$pid"
+    criu dump --ext-unix-sk --file-locks --images-dir /mnt/gentoo/var/lib/criu --shell-job --tree "$pid"
 
     exit 1
 else
