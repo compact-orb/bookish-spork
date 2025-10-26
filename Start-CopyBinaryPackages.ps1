be55a5efedb43b66f65084b0919569ea209fc113
compact-orb <compact-orb@outlook.com>
2025-10-26 23:50:15 +0000

    Update

--- Start-CopyBinaryPackages.ps1
+++ Start-CopyBinaryPackages.ps1
@@ -13,7 +13,7 @@ $PSNativeCommandUseErrorActionPreference = $true
 if ($From) {
     New-Item -Path /mnt/gentoo/var/cache/binpkgs-lowerdir, /mnt/gentoo/var/cache/binpkgs-upperdir, /mnt/gentoo/var/cache/binpkgs-workdir -ItemType Directory
 
-    ./Copy-BsDirectory.ps1 -Path "/$env:CONFIG_PREFIX" -Destination "/mnt/gentoo/var/cache/binpkgs-lowerdir" -FromBs -ThrottleLimit 8
+    ./Copy-BsDirectory.ps1 -Path "/$env:CONFIG_PREFIX" -Destination "/mnt/gentoo/var/cache/binpkgs-lowerdir" -FromBs
 
     mount --types overlay overlay --options lowerdir=/mnt/gentoo/var/cache/binpkgs-lowerdir,upperdir=/mnt/gentoo/var/cache/binpkgs-upperdir,workdir=/mnt/gentoo/var/cache/binpkgs-workdir /mnt/gentoo/var/cache/binpkgs
 } elseif ($To) {
@@ -23,7 +23,7 @@ if ($From) {
         $binpkgsPath = "/mnt/gentoo/var/cache/binpkgs-upperdir"
     }
 
-    ./Copy-BsDirectory.ps1 -Path "$binpkgsPath" -Destination "/$env:CONFIG_PREFIX" -ToBs -ThrottleLimit 8
+    ./Copy-BsDirectory.ps1 -Path "$binpkgsPath" -Destination "/$env:CONFIG_PREFIX" -ToBs
 
     if ($NoOverlay) {
         Remove-Item -Path /mnt/gentoo/var/cache/binpkgs/* -Recurse
