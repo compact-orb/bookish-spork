b4e5055a4cccc1d8cba6a509430a2afbaf316135
compact-orb <compact-orb@outlook.com>
2025-10-26 13:46:35 +0000

    Update

--- install-powershell.sh
+++ install-powershell.sh
@@ -2,13 +2,9 @@ set -e
 
 source ./variables.sh
 
-aria2c --dir=/tmp https://github.com/PowerShell/PowerShell/releases/download/v7.$POWERSHELL_7_VERSION/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
-
 mkdir --parents /opt/microsoft/powershell/7
 
-tar --directory=/opt/microsoft/powershell/7 --extract --file=/tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
-
-rm /tmp/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz
+curl --location --silent https://github.com/PowerShell/PowerShell/releases/download/v7.$POWERSHELL_7_VERSION/powershell-7.$POWERSHELL_7_VERSION-linux-$POWERSHELL_7_ARCHITECTURE.tar.gz | tar --directory=/opt/microsoft/powershell/7 --extract --file=- --gzip
 
 chmod +x /opt/microsoft/powershell/7/pwsh
 
