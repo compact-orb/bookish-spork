32bc4245b54a0d41fced14e8127e6dae0ac5f150
compact-orb <compact-orb@outlook.com>
2025-10-26 23:58:59 +0000

    Update

--- Copy-BsDirectory.ps1
+++ Copy-BsDirectory.ps1
@@ -44,7 +44,7 @@ function FromBs {
                     FromBs -Path "$Path/$($_.ObjectName)" -Destination "$Destination/$($_.ObjectName)"
                 } elseif (($null -ne $_.IsDirectory) -and (-not $_.IsDirectory)) {
                     try {
-                        Invoke-WebRequest -Uri "https://$($env:BUNNY_STORAGE_ENDPOINT)/$Path/$($_.ObjectName)" -Headers @{ accept='*/*'; accesskey=$env:BUNNY_STORAGE_ACCESS_KEY } -OutFile (Join-Path -Path $Destination -ChildPath $_.ObjectName)
+                        Invoke-WebRequest -Uri "https://$($env:BUNNY_STORAGE_ENDPOINT)/$Path/$($_.ObjectName)" -Headers @{ accept='*/*'; accesskey=$env:BUNNY_STORAGE_ACCESS_KEY } -MaximumRetryCount $MaximumRetryCount -RetryIntervalSec $RetryIntervalSec -OutFile (Join-Path -Path $Destination -ChildPath $_.ObjectName)
 
                         Write-Host -Object "Copied $($_.ObjectName)"
                     } catch {
