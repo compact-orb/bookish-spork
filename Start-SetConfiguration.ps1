647663c74b6c7eeaf8c620317d3165e7c27c5f74
compact-orb <compact-orb@outlook.com>
2025-11-30 08:40:33 +0000

    Update SSH key configuration and repository settings in workflows and scripts

--- Start-SetConfiguration.ps1
+++ Start-SetConfiguration.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "Stop"
 
 $PSNativeCommandUseErrorActionPreference = $true
 
-Remove-Item -Path /mnt/gentoo/etc/kernel/config.d, /mnt/gentoo/etc/portage/binrepos.conf, /mnt/gentoo/etc/portage/env, /mnt/gentoo/etc/portage/package.accept_keywords, /mnt/gentoo/etc/portage/package.env, /mnt/gentoo/etc/portage/package.unmask, /mnt/gentoo/etc/portage/package.use, /mnt/gentoo/etc/portage/patches, /mnt/gentoo/etc/portage/repos.conf -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path /mnt/gentoo/etc/kernel/config.d, /mnt/gentoo/etc/portage/binrepos.conf, /mnt/gentoo/etc/portage/env, /mnt/gentoo/etc/portage/package.accept_keywords, /mnt/gentoo/etc/portage/package.env, /mnt/gentoo/etc/portage/package.unmask, /mnt/gentoo/etc/portage/package.use, /mnt/gentoo/etc/portage/patches, /mnt/gentoo/etc/portage/repos.conf /mnt/gentoo/root/.ssh -Recurse -Force -ErrorAction SilentlyContinue
 
 Copy-Item -Path $env:CONFIG_PREFIX/* -Destination /mnt/gentoo -Recurse -Force
 
@@ -28,5 +28,20 @@ BINPKG_COMPRESS="zstd"
 BINPKG_COMPRESS_FLAGS="-19 -T8 --long"
 '@
 }
+
+New-Item -Path "/mnt/gentoo/root/.ssh" -ItemType Directory -Force | Out-Null
+
+chmod 700 "/mnt/gentoo/root/.ssh"
+
+$env:REDESIGNED_BROCCOLI_SSH_KEY | base64 --decode | Set-Content -Path "/mnt/gentoo/root/.ssh/redesigned-broccoli"
+
+chmod 600 "/mnt/gentoo/root/.ssh/redesigned-broccoli"
+
+Set-Content -Path "/mnt/gentoo/root/.ssh/config" -Value @'
+Host github.com
+    IdentityFile ~/.ssh/redesigned-broccoli
+'@
+
+chmod 600 "/mnt/gentoo/root/.ssh/config"
 
 chroot /mnt/gentoo env-update
