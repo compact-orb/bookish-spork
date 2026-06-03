c1e07ec4028610ab24261932eb60d52c196668dc
compact-orb <compact-orb@outlook.com>
2026-06-03 17:04:04 +0800

    fix: include llvm-core/clang in bootstrap emerge command to resolve dependency resolution issues

--- scripts/BuildPackages.sh
+++ scripts/BuildPackages.sh
@@ -155,7 +155,7 @@ case $bootstrap in
 
         FEATURES="binpkg-request-signature" emerge --binpkg-respect-use=n --getbinpkgonly --nodeps dev-lang/rust dev-vcs/git llvm-core/clang
 
-        emerge --buildpkg=n --getbinpkg --oneshot llvm-core/clang-common llvm-core/clang-linker-config llvm-runtimes/clang-runtime
+        emerge --buildpkg=n --oneshot llvm-core/clang llvm-core/clang-common llvm-core/clang-linker-config llvm-runtimes/clang-runtime
 
         rm /etc/portage/package.env/bootstrap
 
