20c7a67bfc7bc90081c11c9f128299fb1a87b502
compact-orb <compact-orb@outlook.com>
2026-06-08 18:50:50 +0800

    chore: remove bootstrap-no-polly package.env configuration

--- scripts/BuildPackages.sh
+++ scripts/BuildPackages.sh
@@ -153,8 +153,6 @@ case $bootstrap in
 
         write_file /etc/portage/package.env/bootstrap "*/* gcc.conf"
 
-        write_file /etc/portage/package.env/bootstrap-no-polly "*/* clang-no-polly.conf"
-
         emerge --buildpkg=n --oneshot llvm-core/llvm llvm-core/clang llvm-core/clang-common llvm-core/clang-linker-config llvm-runtimes/clang-runtime
 
         FEATURES="binpkg-request-signature" emerge --binpkg-respect-use=n --getbinpkgonly --nodeps dev-lang/rust dev-vcs/git
@@ -175,8 +173,6 @@ case $bootstrap in
         rm --force --recursive /var/cache/binhost/* /var/cache/binpkgs/*
 
         rm /etc/portage/package.use/bootstrap
-
-        rm /etc/portage/package.env/bootstrap-no-polly
         ;;
     2)
         # Bootstrap Step 2: System Rebuild
