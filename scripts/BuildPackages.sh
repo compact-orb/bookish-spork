a84852cc43a315613cca49891b03e55dd2a81489
compact-orb <compact-orb@outlook.com>
2026-06-09 04:21:20 +0800

    Refactor llvm-core emerge commands in BuildPackages.sh
    Split emerge command for llvm-core packages into separate lines for clarity.

--- scripts/BuildPackages.sh
+++ scripts/BuildPackages.sh
@@ -153,7 +153,9 @@ case $bootstrap in
 
         write_file /etc/portage/package.env/bootstrap "*/* gcc.conf"
 
-        emerge --buildpkg=n --oneshot llvm-core/llvm llvm-core/clang llvm-core/clang-common llvm-core/clang-linker-config llvm-runtimes/clang-runtime
+        emerge --buildpkg=n llvm-core/llvm llvm-core/clang
+
+        emerge --buildpkg=n --oneshot llvm-core/clang-common llvm-core/clang-linker-config llvm-runtimes/clang-runtime
 
         FEATURES="binpkg-request-signature" emerge --binpkg-respect-use=n --getbinpkgonly --nodeps dev-lang/rust dev-vcs/git
 
